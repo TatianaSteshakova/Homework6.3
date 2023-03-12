284c66d36c4775c9cfdbaea75290973e38d4858d
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-12 22:20:22 +0300

    Add files via upload

---
+++ Homework6.3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+
+    char c[100];
+
+    cout << "Введите слово: " << endl;
+    cin >> c;
+
+    char cd;
+
+    cout << "Второй символ: " << c[1] << endl;
+    cout << "Последний символ:" << c[strlen(c)-1] << endl;
+    cout << "Введите символ для удаления: ";
+    cin >> cd;
+    for (int i = 0; i < strlen(c); i++)
+    {
+        if (c[i] != cd)
+        {
+            printf("%c", c[i]);
+        }
+    }
+}
